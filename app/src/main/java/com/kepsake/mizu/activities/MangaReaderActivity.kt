17abed012c94419f37e9f7143036a89b916f3a63
Rohan Biswas <rohan.biswas@greatlearning.in>
2025-03-14 01:45:05 +0530

    refactor

--- app/src/main/java/com/kepsake/mizu/activities/MangaReaderActivity.kt
+++ app/src/main/java/com/kepsake/mizu/activities/MangaReaderActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 
-import com.kepsake.mizu.ui.ImageGrid
 import com.kepsake.mizu.ui.MangaView
 import com.kepsake.mizu.ui.theme.MizuTheme
 import com.kepsake.mizu.utils.getImages
